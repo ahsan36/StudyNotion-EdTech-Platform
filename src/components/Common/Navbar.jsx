3fc352c5dbf20e71d0074639be03ec7d036a12e9
Ahsan <ahsaninfo1@gmail.com>
2024-06-13 16:32:52 +0530

    Update Navbar

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -145,7 +145,7 @@ function Navbar() {
           {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
             <Link to="/dashboard/cart" className="relative">
 
-              <AiOutlineShoppingCart className="text-2xl text-richblack-100 md:block" />
+              <AiOutlineShoppingCart className="hidden text-2xl text-richblack-100 md:block" />
 
               {totalItems > 0 && (
                 <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
